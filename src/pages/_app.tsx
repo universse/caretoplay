e833e883854ee8ed40fbf746bc89127d3e5ba233
Ulrjch <phuoc317049@gmail.com>
2021-01-12 20:02:06 +0800

    Add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,14 @@ import Router from 'next/router'
 import type { AppProps } from 'next/app'
 import 'focus-visible'
 
+import { log } from 'utils/analytics'
 import SEO from 'components/SEO'
 import 'styles/index.scss'
 
 import { apiClient } from 'utils/apiClient'
 
 Router.events.on('routeChangeComplete', (url, { shallow }) => {
-  console.log(url)
+  !shallow && log('view page', { url })
 })
 
 if (typeof window === 'object') {
@@ -16,7 +17,7 @@ if (typeof window === 'object') {
   // inspect({ iframe: false })
 }
 
-export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+export default function _App({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
       <SEO />
--- src/pages/api/snap.ts
+++ src/pages/api/snap.ts
@@ -13,26 +13,61 @@ export default async function complete(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
-  const { quizSetKey, type } = req.body
+  if (req.body.type === 'stream') {
+    const { events, session } = req.body
 
-  const path = quizSetKey
-    ? `quizSets/${quizSetKey}/${type}`
-    : `overview/${type}`
+    let pageCount = 0
+    const eventCount = events.length
+    const startedAt = events[0].timestamp
+    const duration = events[eventCount - 1].timestamp - startedAt
 
-  return new Promise((resolve) => {
-    firebase
-      .database()
-      .ref(`stats/${path}Count`)
-      .transaction(
-        (count) => count + 1,
-        (error) => {
-          if (error) {
-            res.status(500).json({ success: false })
-          } else {
-            res.status(200).json({ success: true })
+    for (const event of events) {
+      if (event.type === 'view page') {
+        pageCount = pageCount + 1
+      }
+
+      event.timestamp = event.timestamp / 1000
+    }
+
+    session.createdAt = new Date(session.createdAt).toUTCString()
+    session.startedAt = new Date(startedAt).toUTCString()
+    session.duration = duration / 1000
+    session.pageCount = pageCount
+    session.latency = session.latency / 1000
+    session.pageLoad = session.pageLoad / 1000
+    session.eventCount = eventCount
+    session._timestamp = firebase.database.ServerValue.TIMESTAMP
+    session.events = events
+
+    try {
+      await firebase.database().ref('stats/sessions').push(session)
+
+      res.status(200).json({ success: true })
+    } catch {
+      res.status(500).json({ success: false })
+    }
+  } else {
+    const { quizSetKey, type } = req.body
+
+    const path = quizSetKey
+      ? `quizSets/${quizSetKey}/${type}`
+      : `overview/${type}`
+
+    return new Promise((resolve) => {
+      firebase
+        .database()
+        .ref(`stats/${path}Count`)
+        .transaction(
+          (count) => count + 1,
+          (error) => {
+            if (error) {
+              res.status(500).json({ success: false })
+            } else {
+              res.status(200).json({ success: true })
+            }
+            resolve()
           }
-          resolve()
-        }
-      )
-  })
+        )
+    })
+  }
 }
